d74b41fae280814b6c4cb67b9b7a9b2f491a40d8
Bible's MacBook Pro <comim_it02@COMIM-IT02ui-MacBookPro.local>
2024-02-19 14:09:42 +0900

    forth commit

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-let box = document.querySelector(".container");
-let RandomImage = document.querySelector(".container");
+let box = document.querySelector("body");
+let RandomImage = document.querySelector(".container img");
 
 let nowSrc;
 let i = 0;
 let toggle = true;
 
+box.addEventListener("mouseup", replay);
+// box.addEventListener("touchstart", replay);
+box.addEventListener("mousedown", pause);
+// box.addEventListener("touchend", pause);
+
 function change() {
-  if (i < 10) {
+  if (i < 11) {
     i++;
-    RandomImage.style.backgroundImage = `url(./images/cat${i}.jpeg)`;
-    // console.log(i);
-  } else if (i >= 10) {
+    RandomImage.src = `./images/cat${i}.jpg`;
+  } else if (i >= 11) {
     i = 1;
-    RandomImage.style.backgroundImage = `url(./images/cat${i}.jpeg)`;
+    RandomImage.src = `./images/cat${i}.jpg`;
   }
-  nowSrc = RandomImage.style.backgroundImage;
+  nowSrc = RandomImage.src;
+  console.log(i);
 }
 
-let pauseTiming = setInterval(change, 100);
+let pauseTiming = setInterval(change, 80);
 
 function pause() {
   clearInterval(pauseTiming);
-  RandomImage.style.backgroundImage = nowSrc;
+  RandomImage.src = nowSrc;
   toggle = false;
 }
 
 function replay() {
-  pauseTiming = setInterval(change, 100);
+  pauseTiming = setInterval(change, 80);
   toggle = true;
 }
 
-box.addEventListener("mouseup", replay);
-box.addEventListener("mousedown", pause);
+console.log(box);
